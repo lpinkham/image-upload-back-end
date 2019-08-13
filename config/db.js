ba68541452ff8c61dac758300868b1c86ddeae49
lpinkham <lori_pinkham@comcast.net>
2019-08-13 12:24:30 -0400

    Commit to remove team references to heroku and AWS

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'the-coherent-chickens-express'
+const mongooseBaseName = 'image-upload'
 
 // create the mongodb uri for development and test
 const database = {
